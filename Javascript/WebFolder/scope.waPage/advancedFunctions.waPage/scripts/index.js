2f9a5dc872406b53de7496882c061615872df877
David Robbins <dave@wakanda.org>
2012-10-08 17:26:52 -0700

    added advanced functions section to Javascript 101.

---
+++ Javascript/WebFolder/scope.waPage/advancedFunctions.waPage/scripts/index.js
@@ -0,0 +1,26 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		//Closures.
+		function myFunction(arg1) {
+			return function() {
+				alert(arg1);
+			}
+		}
+		
+		var fn = myFunction("Hello Closure.");
+		fn();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
