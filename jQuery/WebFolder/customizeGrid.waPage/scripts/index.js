44a2d266908fdfc62657959063e9385f1ef47083
David Robbins <dave@wakanda.org>
2012-10-26 06:27:26 -0700

    added in mickey mantle

---
+++ jQuery/WebFolder/customizeGrid.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		//Hide Sort Icons
+		$("#dataGrid1 .waf-sort").hide(); //hide sort indicators
+		//$("#dataGrid1 .waf-dataGrid-col-1 .waf-sort").hide() //just for the second column
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
